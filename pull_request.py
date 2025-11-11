3c5651a8859bfaac6726367f870bc42723b60356
Victor <pnthang@gmail.com>
2025-11-11 17:23:20 -0600

    AutoFix issue #2: Bug with the new function

---
+++ pull_request.py
@@ -0,0 +1,4 @@
+
+- In the `get_commits` function, add handling for the case where the branch passed in is the same as the current branch.
+- In the `create_pull_request` function, add a check to ensure that the `commits` list is not empty.
+
